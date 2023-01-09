d93f7dc3e3795fef5991031be307e8b5e26914f8
Shahid <i_m_shahid@yahoo.com>
2023-01-09 13:18:54 +0530

    initial Commit

---
+++ src/main/java/org/greatlearning/dispatcher/ServletInitializer.java
@@ -0,0 +1,26 @@
+package org.greatlearning.dispatcher;
+
+import org.greatlearning.config.SpringConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[0];
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {SpringConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+
+}
